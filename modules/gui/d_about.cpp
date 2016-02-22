676a133a422afb506c50c7b6b8169d264bd89ca4
Ivan Feofanov <raketomania@yandex.ru>
2016-02-22 16:34:55 +0300

    Оформил диалог "О программе"

--- modules/gui/d_about.cpp
+++ modules/gui/d_about.cpp
@@ -1,11 +1,17 @@
 #include "d_about.h"
 #include "ui_d_about.h"
 
+#include "include/version.h"
+
 About::About(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::About)
 {
     ui->setupUi(this);
+    QString nameAndVersion = QString("<html><center><b> %1 </b> <br> %2 </center></html>")
+                             .arg(qApp->applicationName(),
+                                  VERSION);
+    ui->labelName->setText(nameAndVersion);
 }
 
 About::~About()
